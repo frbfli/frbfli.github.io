b0706b78bd9e4ae52667b48514353b6257b16beb
frbfli@gmail.com <frbfli@gmail.com>
2021-07-11 16:49:26 -0500

    Added Covenant Images

--- GroundCommand/config/covenant.js
+++ GroundCommand/config/covenant.js
@@ -3,36 +3,159 @@ var Covenant_Units = {
 		'name':'Grunt Levies',
 		'cost':'80',
 		'buildRating':'1',
-		'type':'I'
+		'type':'I',
+		'icon':'gruntLevies',
+		'upgrades': {
+			'0': {
+				'name':'Elite Minor Overseer',
+				'cost':'20'
+			},
+			'1': {
+				'name':'Fuel Rod Cannons',
+				'cost':'15'
+			}
+		}
 	},
 	'1': {
+		'name':'Elite Assault',
+		'cost':'3',
+		'buildRating':'2',
+		'type':'I',
+		'icon':'eliteAssault'
+	},
+	'2': {
+		'name':'Elite General',
+		'cost':'175',
+		'buildRating':'4',
+		'type':'I',
+		'icon':'eliteGeneral'
+	},
+	'3': {
+		'name':'Elite Officer',
+		'cost':'100',
+		'buildRating':'2',
+		'type':'I',
+		'icon':'eliteOfficer'
+	},
+	'4': {
+		'name':'Elite Ultra',
+		'cost':'180',
+		'buildRating':'3',
+		'type':'I',
+		'icon':'eliteUltra',
+		'upgrades':{
+			'0': {
+				'name':'Officer',
+				'cost':'30'
+			}
+		},
+		'perks': {
+			'0': {
+				'name':'Death from Above',
+				'cost':'20'
+			}
+		}
+	},
+	'5': {
 		'name':'Elite Zealot',
 		'cost':'125',
 		'buildRating':'3',
-		'type':'I'
+		'type':'I',
+		'icon':'eliteZealot'
 	},
-	'2': {
+	'6': {
+		'name':'Jackal Assault',
+		'cost':'120',
+		'buildRating':'1',
+		'type':'I',
+		'icon':'jackalAssault'
+	},
+	'7': {
+		'name':'Jackal Marksmen',
+		'cost':'120',
+		'buildRating':'2',
+		'type':'I',
+		'icon':'jackalMarksmen'
+	},
+	'8': {
+		'name':'Scout Ghost',
+		'cost':'80',
+		'buildRating':'1',
+		'type':'A',
+		'icon':'scoutGhost',
+		'elementCap':'3'
+	},
+	'9': {
+		'name':'Storm Ghost',
+		'cost':'100',
+		'buildRating':'2',
+		'type':'A',
+		'icon':'stormGhost',
+		'elementCap':'3'
+	},
+	'10': {
 		'name':'Hunters',
 		'cost':'240',
 		'buildRating':'4',
-		'type':'A'
+		'type':'A',
+		'icon':'hunters'
 	},
-	'3': {
-		'name':'Ghost (Single Element)',
-		'cost':'80',
-		'buildRating':'1',
-		'type':'A'
+	'11': {
+		'name':'Artillery Wraith',
+		'cost':'250',
+		'buildRating':'4',
+		'type':'A',
+		'icon':'artilleryWraith'
 	},
-	'4': {
-		'name':'Ghost (Two Elements)',
-		'cost':'160',
+	'12': {
+		'name':'Anti-Air Wraith',
+		'cost':'250',
+		'buildRating':'4',
+		'type':'A',
+		'icon':'aaWraith'
+	},
+	'13': {
+		'name':'Revenant',
+		'cost':'140',
 		'buildRating':'2',
-		'type':'A'
+		'type':'A',
+		'icon':'revenant',
+		'elementCap':'2'
 	},
-	'5': {
-		'name':'Ghost (Three Elements)',
-		'cost':'240',
+	'14': {
+		'name':'Locust',
+		'cost':'250',
+		'buildRating':'4',
+		'type':'A',
+		'icon':'locust'
+	},
+	'15': {
+		'name':'Banshee',
+		'cost':'120',
+		'buildRating':'2',
+		'type':'F',
+		'icon':'banshee',
+		'elementCap':'2'
+	},
+	'16': {
+		'name':'Phantom',
+		'cost':'250',
+		'buildRating':'',
+		'type':'F',
+		'icon':'phantom'
+	},
+	'17': {
+		'name':'Seraph',
+		'cost':'180',
 		'buildRating':'3',
-		'type':'A'
+		'type':'F',
+		'icon':'seraph'
+	},
+	'18': {
+		'name':'Vampire',
+		'cost':'250',
+		'buildRating':'4',
+		'type':'F',
+		'icon':'vampire'
 	}
 }
